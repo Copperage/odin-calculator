7bf02eb022219fd5356e48a49822961269106519
Ethan <ethanhud6@gmail.com>
2022-05-04 16:18:35 +0100

    Added basic calculator js need to add QoL features like keyboard inputs etc.

--- script.js
+++ script.js
@@ -1,36 +1,117 @@
-// Arithmetic Operations
+// Calculator Functionality
 
-function add(num1, num2) {
-	return num1 + num2;
-}
+class Calculator {
+	constructor(prevOpText, currentOpText) {
+		this.prevOpText = currentOpText;
+		this.currentOpText = currentOpText;
+		this.clear();
+	}
 
-function subtract(num1, num2) {
-	return num1 - num2;
-}
+	clear() {
+		this.prevOp = '';
+		this.currentOp = '';
+		this.operation = undefined;
+	}
 
-function multiply(num1, num2) {
-	return num1 * num2;
-}
+	delete() {
+		this.currentOp = this.currentOp.toString().slice(0, -1);
+	}
 
-function divide(num1, num2) {
-	return num1 / num2;
-}
+	appendNum(number) {
+		if (number === '.' && this.currentOp.includes('.')) return;
+		this.currentOp = this.currentOp.toString() + number.toString();
+	}
 
-function operate(num1, operator, num2) {
-	firstNum = Number(num1);
-	secondNum = Number(num2);
+	chooseOperation(operation) {
+		if (this.currentOp === '') return;
+		if (this.currentOp !== '') {
+			this.compute();
+		}
 
-	switch (operator) {
-		case '+':
-			return add(firstNum, secondNum);
-		case '-':
-			return subtract(firstNum, secondNum);
-		case 'x':
-			return multiply(firstNum, secondNum);
-		case '÷':
-			if (firstNum === 0 || secondNum === 0) return null;
-			else return divide(firstNum, secondNum);
-		default:
-			return null;
+		this.operation = operation;
+		this.prevOp = this.currentOp;
+		this.currentOp = '';
+	}
+
+	compute() {
+		let computation;
+		const prev = parseFloat(this.prevOp);
+		const curr = parseFloat(this.currentOp);
+
+		function divide(prev, curr) {
+			if (prev === 0 || curr === 0) computation = null;
+			else return prev / curr;
+		}
+
+		switch (this.operation) {
+			case '+':
+				computation = prev + curr;
+				break;
+			case '-':
+				computation = prev - curr;
+				break;
+			case '*':
+				computation = prev * curr;
+				break;
+			case '÷':
+				computation = divide(prev, curr);
+				break;
+			default:
+				return;
+		}
+
+		this.currentOp = computation;
+		this.prevOp = '';
+		this.operator = undefined;
+	}
+
+	updateDisplay() {
+		this.currentOpText.innerText = this.currentOp;
 	}
 }
+
+// Query Selections + Init
+const numButtons = document.querySelectorAll('[data-number]');
+const opButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const acButton = document.querySelector('[data-all-clear]');
+const delButton = document.querySelector('[data-delete]');
+const currentOpText = document.querySelector('[data-current-op]');
+const prevOpText = document.querySelector('[data-prev-op]');
+
+const calculator = new Calculator(prevOpText, currentOpText);
+
+this.currentOp = '';
+this.prevOp = '';
+this.operator = undefined;
+
+// Event Listeners
+
+numButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		calculator.appendNum(button.innerText);
+		calculator.updateDisplay();
+	});
+});
+
+opButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		calculator.chooseOperation(button.innerText);
+		calculator.updateDisplay();
+	});
+});
+
+equalsButton.addEventListener('click', () => {
+	calculator.compute();
+	calculator.updateDisplay();
+});
+
+acButton.addEventListener('click', () => {
+	calculator.clear();
+	calculator.updateDisplay();
+});
+
+delButton.addEventListener('click', () => {
+	calculator.delete();
+	calculator.updateDisplay();
+});
